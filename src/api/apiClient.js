2699b65dffad5445f53b684c3043e2b7dbb7f022
ThyMuoyhak <thymuoyhak.biu@gmail.com>
2025-09-28 00:07:04 +0700

    up[date

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,18 +2,21 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://khmer365-1.onrender.com', // Updated to Render URL
 });
 
 // Add request interceptor to include auth token
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default apiClient;
